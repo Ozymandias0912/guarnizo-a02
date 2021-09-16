d45adbbf219e58085c70abee349db9c9b6307eb9
Juan <jdavid29a@gmail.com>
2021-09-16 09:23:06 -0400

    added exercise11

---
+++ exercise11/src/main/java/Solution11.java
@@ -0,0 +1,40 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution11
+ *  Copyright 2021 Juan Guarnizo
+ */
+
+/* Pseudocode
+Create scanner variable
+
+Print to the screen: "How many euros are you exchanging? "
+Save the input in "euros"
+
+Print to the screen: "What is the exchange rate? "
+Save the input in "rate"
+
+Create "dollars" = euros * rate
+
+Print to the screen:" "euros" euros at an exchange rate of "rate" is "
+Print to the screen:" "dollars" U.S. dollars. "
+
+ */
+
+
+import java.util.Scanner;
+
+public class Solution11 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("How many euros are you exchanging? ");
+        double euros = input.nextDouble();
+
+        System.out.println("What is the exchange rate? ");
+        double rate = input.nextDouble();
+
+        double dollars = euros * rate;
+
+        System.out.printf(" %.2f euros at an exchange rate of %.2f is \n", euros, rate);
+        System.out.printf(" %.2f U.S. dollars. ", dollars);
+    }
+}
