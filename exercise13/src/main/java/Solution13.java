d3a7fc50a9a3149932963fd4f3dcdb92406a8114
Juan <jdavid29a@gmail.com>
2021-09-17 12:17:48 -0400

    modified exercise13

--- exercise13/src/main/java/Solution13.java
+++ exercise13/src/main/java/Solution13.java
@@ -5,7 +5,8 @@
 
 import java.util.Scanner;
 
-import static java.lang.Math.ceil;
+
+import static java.lang.Math.floor;
 
 
 /*
@@ -51,7 +52,7 @@ public class Solution13 {
 
         double amount;
         amount = principal * (Math.pow((1 + (rate / 100) / period), (period * time)));
-        double amountRounded = ceil(amount * 100 + 0.5) / 100;//rounding up to the nearest cent
+        double amountRounded = floor(amount * 100 + 0.5) / 100;//rounding up to the nearest cent
 
         System.out.printf("$%.2f invested at %.1f%% for %.1f years compounded %.0f times per year is $%.2f",
                 principal, rate, time, period, amountRounded);
